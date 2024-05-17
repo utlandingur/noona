df09df8cc94806ff494621bb2731cdc2eb775761
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-05-17 18:13:48 +0000

    update spacing

--- components/CampaignSection.tsx
+++ components/CampaignSection.tsx
@@ -1,14 +1,11 @@
-import { Stack } from '@mui/material';
 import CampaignVariableSelector from './CampaignVariableSelector';
 const ContainerSection = () => {
 	return (
-		<>
-			<Stack direction={'row'} spacing={10}>
-				<CampaignVariableSelector type="NEW" />
-				<CampaignVariableSelector type="RISK" />
-				<CampaignVariableSelector type="LOST" />
-			</Stack>
-		</>
+		<div className="flex gap-8">
+			<CampaignVariableSelector type="NEW" />
+			<CampaignVariableSelector type="RISK" />
+			<CampaignVariableSelector type="LOST" />
+		</div>
 	);
 };
 
